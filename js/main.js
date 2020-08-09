3ccab0259a64d68bfe7d90442e5f80fb6fda8478
Bryan Vera <bveracachay@gmail.com>
2020-08-09 12:51:05 -0500

    se implemento la posibilidad de pasar la actividades pendienta a la seccion de completadas.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 let form = document.querySelector('form');
-let container = document.getElementById('container');
+let container = document.getElementById('container-pendientes');
+let containerC = document.getElementById('container-completas');
 
-form.onsubmit = function(event) {
-    event.preventDefault();
-    let tarea = this.querySelector('.tarea').value;
-    console.log(tarea);
-    let article = document.createElement('article');
-    console.log(article);
-    article.classList.add('list');
-    article.innerHTML = `
-            <ul>
-                <li>${tarea}</li>
-                <li><button>X</button></li>
-                <li><button>Ok</button></li>
-            </ul>
-    `;
 
-    article.querySelector('button').onclick = function() {
-        this.closest('.list').remove();
+let btn = form.querySelector('button');
+
+btn.onclick = function() {
+    form.onsubmit = function(event) {
+        event.preventDefault();
+        let tarea = this.querySelector('.tarea').value;
+        console.log(tarea);
+        let article = document.createElement('article');
+        console.log(article);
+        article.classList.add('list');
+        article.innerHTML = `
+                <ul>
+                    <li>${tarea}</li>
+                    <li><button id="btn-borrar">Eliminar</button></li>
+                    <li><button id="btn-check">Completado</button></li>
+                </ul>
+        `;
+        container.appendChild(article);
+        let ul = article.querySelector('li');
+
+        btnA = article.querySelectorAll('button');
+
+        btnA.forEach(btn => {
+            btn.addEventListener('click', EventButton);
+        });
+
+        function EventButton(event) {
+            if (event.target.id == 'btn-borrar') {
+                this.closest('.list').remove();
+            } else if (event.target.id == 'btn-check') {
+                ul.classList.add('rayar');
+                event.target.classList.add('completar');
+                containerC.appendChild(article);
+            }
+            console.log(event.target);
+        }
+
     }
-
-    container.appendChild(article);
 }
+
+
+
+
+
+// // btnT.onclick = function() {
+// //     btnT.classList.toggle('btn-t');
+// // }
+
+// article.querySelector('button').onclick = function() {
+//         this.closest('.list').remove();
+//     }
+//     // article.querySelector('button').onclick = function() {
+//     //     this.classList.toggle('is-active');
+//     // }
